35159022fc18aefce991dcd7c501147cf731bb0f
Alex Carney <alcarneyme@gmail.com>
2020-08-08 13:50:45 +0100

    Very rough, initial attempt a fountain syntax highlighting

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as monaco from 'monaco-editor'
+import { EXAMPLE_SCRIPT } from "./script";
 import "./index.css"
 
 // @ts-ignore
@@ -8,8 +9,33 @@ self.MonacoEnvironment = {
   }
 }
 
+monaco.languages.register({ id: 'fountain' })
+monaco.languages.setMonarchTokensProvider('fountain', {
+  tokenizer: {
+    root: [
+      { include: '@sceneHeadings' },
+      { include: '@transitions' },
+      { include: '@characters' }
+    ],
+
+    sceneHeadings: [
+      [/^([eEiI][nNxX][tT])?\..*$/, 'invalid']
+    ],
+
+    characters: [
+      [/^[A-Z ]+\^?$/, 'type']
+    ],
+
+    transitions: [
+      [/>[^<]+$/, 'string'],
+      [/[A-Z ]+TO:/, 'string']
+    ]
+  }
+})
+
 monaco.editor.create(document.body, {
-  value: "Hello!",
+  value: EXAMPLE_SCRIPT,
+  language: 'fountain',
   lineNumbers: "off",
   minimap: { enabled: false }
 })
