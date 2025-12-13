3211a9e23ddb6c26786c0e229e850f3798d627aa
Alex Carney <alcarneyme@gmail.com>
2025-12-13 20:03:45 +0000

    awdur: Add test for multiple consecutive blocks

--- lib/awdur/tests/cli/test_extract.py
+++ lib/awdur/tests/cli/test_extract.py
@@ -20,3 +20,27 @@ def test_extract_hello_world(workspace: pathlib.Path):
     assert output.exists()
 
     assert 'print("Hello, World!")\n' == output.read_text()
+
+
+@pytest.mark.parametrize("workspace", ["multiple-blocks"], indirect=True)
+def test_extract_multiple_blocks(workspace: pathlib.Path):
+    """Ensure we can extract code from the example correctly."""
+
+    result = subprocess.run(
+        [sys.executable, "-m", "awdur", "extract", "multiple-blocks.rst"], cwd=workspace
+    )
+    assert result.returncode == 0
+
+    output = workspace / "multiple-blocks.py"
+    assert output.exists()
+
+    result = subprocess.run([sys.executable, f"{output}"], capture_output=True)
+    assert result.returncode == 0
+
+    # fmt: off
+    assert  result.stdout.decode("utf-8") == (
+        "A triangle with sides a=3, b=4, c=5 has\n"
+        "- Perimeter, P=12\n"
+        "- Area, A=6.0\n"
+    )
+    # fmt: on
